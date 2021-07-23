5dff6727af00a7e3da05391df7aeee8a1c72b9f4
susant100 <87869242+susant100@users.noreply.github.com>
2021-07-23 21:38:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var winner=["Lipsa","Twinkle","Ashu","Nandu"];
+var winner=["susant","susa","sushi","kalia"];
 function name(player1,player2){
   var player11=player1.slice(0,1).toUpperCase()+player1.slice(1,player1.length).toLowerCase();
   var player22=player2.slice(0,1).toUpperCase()+player2.slice(1,player2.length).toLowerCase();
